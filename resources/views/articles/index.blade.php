08d30e10413c82c79c66d8d8f6840691a6794790
Boaz Crezee <bjcrezee@gmail.com>
2025-02-28 14:56:46 +0100

    Added my portfolio website ported to blade php, with the layout separated, and with the blog post contents loaded from an associative array in web.php

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,32 @@
+<x-layout.main>
+    <section>
+        <article>
+            <div class="textAndImage programsImageBackground firstBar">
+                <div class=textBackground>
+                    <h2>Blog</h2>
+                    <p>
+                        Choose a blog post:
+                    </p>
+                    <ul>
+                        <li>
+                            <a href="./blog/1">My programming biography.</a>
+                        </li>
+
+                        <li>
+                            <a href="./blog/2">Study choice</a>
+                        </li>
+                        <li>
+                            <a href="./blog/3">Personal SWOT analysis</a>
+                        </li>
+                        <li>
+                            <a href="./blog/4">First feedback</a>
+                        </li>
+                        <li>
+                            <a href="./blog/5">the ICT field of work.</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+    </section>
+</x-layout.main>
