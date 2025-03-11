331dd13b50c24a502e95275166fcac75d5b4413e
Boaz Crezee <bjcrezee@gmail.com>
2025-03-11 14:26:11 +0100

    Refactored two more views to use the container component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,57 +1,49 @@
 <x-layout.main>
     <section>
-        <article>
-            <div class="textAndImage programsImageBackground firstBar">
-                <div class=textBackground>
-                    <h2>Why The ICT Field Suits Me</h2>
-                    <p>
-                        I like web development, this field of engineering allows products to be developed relatively
-                        quickly, it allows tools to be made and distributed cheaply to enrich the lives of everyone who owns a computer
-                        and to allow the developer(s) to make living in the process. The ideas I have for software I
-                        want to develop, and my passion for programming, that is my motivation.
-                    </p>
-                    <p>
-                        I like the simplicity and reliability offered by HTML with javascript, it allows
-                        sophiscticated online tools to be made relatively easily.
-                    </p>
-                    <p>
-                        Since my first program in 2018 I have enjoyed programming. I have written many kinds of
-                        programs in many languages, of which:
-                    </p>
-                    <ul>
-                        <li>BASIC,</li>
-                        <li>C,</li>
-                        <li>React,</li>
-                        <li>React Native,</li>
-                        <li>AutoHotKey,</li>
-                        <li>Processing,</li>
-                        <li>Arduino,</li>
-                        <li>Unity,</li>
-                    </ul>
-                    <p>
-                        and of course
-                    </p>
-                    <ul>
-                        <li>HTML,</li>
-                        <li>CSS,</li>
-                        <li>and Javascript.</li>
-                    </ul>
-                </div>
-            </div>
-        </article>
-        <article>
-            <div class="textAndImage codeImageBackground">
-                <div class=textBackground>
-                    <h2>My Biggest Project</h2>
-                    <p>
-                        My biggest project yet is the development of a tool that can generate a personal planning
-                        based on a to do list and an agenda. I started developing it in React Native over two years ago. However I started
-                        again half a year ago, this time I used HTML, CSS, and JavaScript. Hundreds of hours I have
-                        spent on it already, and the first release is getting into view. All main features work, but
-                        it is not that user friendly yet. I recently converted the codebase to use modules instead of 'loose' scripts.
-                    </p>
-                </div>
-            </div>
-        </article>
+        <x-layout.container>
+            <h2>Why The ICT Field Suits Me</h2>
+            <p>
+                I like web development, this field of engineering allows products to be developed relatively
+                quickly, it allows tools to be made and distributed cheaply to enrich the lives of everyone who owns a computer
+                and to allow the developer(s) to make living in the process. The ideas I have for software I
+                want to develop, and my passion for programming, that is my motivation.
+            </p>
+            <p>
+                I like the simplicity and reliability offered by HTML with javascript, it allows
+                sophiscticated online tools to be made relatively easily.
+            </p>
+            <p>
+                Since my first program in 2018 I have enjoyed programming. I have written many kinds of
+                programs in many languages, of which:
+            </p>
+            <ul>
+                <li>BASIC,</li>
+                <li>C,</li>
+                <li>React,</li>
+                <li>React Native,</li>
+                <li>AutoHotKey,</li>
+                <li>Processing,</li>
+                <li>Arduino,</li>
+                <li>Unity,</li>
+            </ul>
+            <p>
+                and of course
+            </p>
+            <ul>
+                <li>HTML,</li>
+                <li>CSS,</li>
+                <li>and Javascript.</li>
+            </ul>
+        </x-layout.container>
+        <x-layout.container>
+            <h2>My Biggest Project</h2>
+            <p>
+                My biggest project yet is the development of a tool that can generate a personal planning
+                based on a to do list and an agenda. I started developing it in React Native over two years ago. However I started
+                again half a year ago, this time I used HTML, CSS, and JavaScript. Hundreds of hours I have
+                spent on it already, and the first release is getting into view. All main features work, but
+                it is not that user friendly yet. I recently converted the codebase to use modules instead of 'loose' scripts.
+            </p>
+        </x-layout.container>
     </section>
 </x-layout.main>
